0b2ae78ad9874059f70242fcdc1d34764646d762
platsaver <kingdomofhellborn123@gmail.com>
2025-05-07 23:24:41 +0700

    Now post can be render styling instead of markdown

--- src/components/ArticleContent.jsx
+++ src/components/ArticleContent.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 const ArticleContent = ({ content }) => {
   return (
     <div className="col-md-8 mx-auto">
       <div className="content">
         {content ? (
-          <div dangerouslySetInnerHTML={{ __html: content }} />
+          <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
         ) : (
           <p>No content available.</p>
         )}
